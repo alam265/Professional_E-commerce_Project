4d97b885bd33789e49ae58cc901fc82cbab95968
alam265 <mda7095y@gmail.com>
2025-09-25 00:57:42 +0600

    Added new feature / fixed bug / updated code

---
+++ src/main/java/com/springproject/profEcomWebApp/ProfEcomWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.springproject.profEcomWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfEcomWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfEcomWebAppApplication.class, args);
+        System.out.println("Server is Running!!");
+	}
+
+}
---
+++ src/test/java/com/springproject/profEcomWebApp/ProfEcomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.profEcomWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfEcomWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
