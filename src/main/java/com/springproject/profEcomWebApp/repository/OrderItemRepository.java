88623c8e01154515ac41c6a460ead8d0f67745d4
alam265 <mda7095y@gmail.com>
2025-11-27 16:29:26 +0600

    Order related repo created

---
+++ src/main/java/com/springproject/profEcomWebApp/repository/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.springproject.profEcomWebApp.repository;
+
+
+import com.springproject.profEcomWebApp.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
