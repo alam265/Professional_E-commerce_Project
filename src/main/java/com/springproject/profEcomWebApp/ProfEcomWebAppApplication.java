2270a2bcec414f68fced77733a826becdb2f9c18
alam265 <mda7095y@gmail.com>
2025-12-23 20:27:53 +0600

    updated main file

--- src/main/java/com/springproject/profEcomWebApp/ProfEcomWebAppApplication.java
+++ src/main/java/com/springproject/profEcomWebApp/ProfEcomWebAppApplication.java
@@ -8,7 +8,7 @@ public class ProfEcomWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfEcomWebAppApplication.class, args);
-        System.out.println("Server is Running!!!");
+        System.out.println("Server is Running....");
 
 	}
 
